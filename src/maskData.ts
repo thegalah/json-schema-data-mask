924cb11fee5da63dbaede28033c0532a342f1e2f
Matthew <thegalah@gmail.com>
2022-06-08 14:24:55 -0700

    Update maskData.ts

--- src/maskData.ts
+++ src/maskData.ts
@@ -7,5 +7,7 @@ interface IMaskOptions {}
 export const maskData = (jsonSchema: Schema, data: unknown, options: IMaskOptions = {}) => {
     const validate = ajv.compile(jsonSchema);
     const valid = validate(data);
-    if (!valid) console.log(validate.errors);
+    if (!valid) {
+        console.log(validate.errors);
+    }
 };
