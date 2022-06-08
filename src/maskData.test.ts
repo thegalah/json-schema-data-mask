8897e078581334c79cad1028a5d7dcc8f9facd24
Matthew <thegalah@gmail.com>
2022-06-08 15:09:41 -0700

    Update maskData.test.ts

--- src/maskData.test.ts
+++ src/maskData.test.ts
@@ -16,18 +16,18 @@ describe("maskData", () => {
         expect(result).toStrictEqual(expected);
     });
 
-    // test("it should mask a single nested property", () => {
-    //     const schema = {
-    //         type: "object",
-    //         properties: {
-    //             foo: { type: "integer" },
-    //         },
-    //         required: ["foo"],
-    //         additionalProperties: false,
-    //     };
-    //     const data = { foo: 123, nestedBar: { a: 456, b: 789 } };
-    //     const result = maskData(schema, data, {});
-    //     const expected = { foo: 123 };
-    //     expect(result).toStrictEqual(expected);
-    // });
+    test("it should mask a single nested property", () => {
+        const schema = {
+            type: "object",
+            properties: {
+                foo: { type: "integer" },
+            },
+            required: ["foo"],
+            additionalProperties: false,
+        };
+        const data = { foo: 123, nestedBar: { a: 456, b: 789 } };
+        const result = maskData(schema, data, {});
+        const expected = { foo: 123 };
+        expect(result).toStrictEqual(expected);
+    });
 });
