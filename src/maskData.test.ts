4307519357c1008dc9680765e1d266f1a07e8507
Matthew <thegalah@gmail.com>
2022-06-08 14:24:05 -0700

    wip

--- src/maskData.test.ts
+++ src/maskData.test.ts
@@ -1,6 +1,18 @@
+import { maskData } from "./maskData";
+
 describe("Some test", () => {
     test("it should be true", () => {
-        const someValue = true;
-        expect(someValue).toBe(true);
+        const schema = {
+            type: "object",
+            properties: {
+                foo: { type: "integer" },
+            },
+            required: ["foo"],
+            additionalProperties: false,
+        };
+        const data = { foo: 123, bar: 456 };
+        const result = maskData(schema, data, {});
+        const expected = { foo: 123 };
+        expect(result).toStrictEqual(expected);
     });
 });
--- src/maskData.ts
+++ src/maskData.ts
@@ -1,1 +1,11 @@
-export const maskData = (jsonSchema, data: unknown) => {};
+import Ajv, { Schema } from "ajv";
+
+const ajv = new Ajv({ allErrors: true });
+
+interface IMaskOptions {}
+
+export const maskData = (jsonSchema: Schema, data: unknown, options: IMaskOptions = {}) => {
+    const validate = ajv.compile(jsonSchema);
+    const valid = validate(data);
+    if (!valid) console.log(validate.errors);
+};
