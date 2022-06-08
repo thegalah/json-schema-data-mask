1b1036b7b02812c38c3e9f6b85adf60b495a0bea
Matthew <thegalah@gmail.com>
2022-06-08 16:42:58 -0700

    Update maskData.test.ts

--- src/maskData.test.ts
+++ src/maskData.test.ts
@@ -1,64 +1,64 @@
 import { maskData } from "./maskData";
 
 describe("maskData", () => {
-    // test("it should mask a single non included property", () => {
-    //     const schema = {
-    //         type: "object",
-    //         properties: {
-    //             foo: { type: "integer" },
-    //         },
-    //         required: ["foo"],
-    //         additionalProperties: false,
-    //     };
-    //     const data = { foo: 123, bar: 456 };
-    //     const result = maskData(schema, data, {});
-    //     const expected = { foo: 123 };
-    //     expect(result).toStrictEqual(expected);
-    // });
+    test("it should mask a single non included property", () => {
+        const schema = {
+            type: "object",
+            properties: {
+                foo: { type: "integer" },
+            },
+            required: ["foo"],
+            additionalProperties: false,
+        };
+        const data = { foo: 123, bar: 456 };
+        const result = maskData(schema, data, {});
+        const expected = { foo: 123 };
+        expect(result).toStrictEqual(expected);
+    });
 
-    // test("it should mask a single nested property", () => {
-    //     const schema = {
-    //         type: "object",
-    //         properties: {
-    //             foo: { type: "integer" },
-    //         },
-    //         required: ["foo"],
-    //         additionalProperties: false,
-    //     };
-    //     const data = { foo: 123, nestedBar: { a: 456, b: 789 } };
-    //     const result = maskData(schema, data, {});
-    //     const expected = { foo: 123 };
-    //     expect(result).toStrictEqual(expected);
-    // });
-    // test("it should mask a property with mismatched type by default", () => {
-    //     const schema = {
-    //         type: "object",
-    //         properties: {
-    //             foo: { type: "string" },
-    //         },
-    //         required: ["foo"],
-    //         additionalProperties: false,
-    //     };
-    //     const data = { foo: 123 };
-    //     const result = maskData(schema, data, {});
-    //     const expected = {};
-    //     expect(result).toStrictEqual(expected);
-    // });
+    test("it should mask a single nested property", () => {
+        const schema = {
+            type: "object",
+            properties: {
+                foo: { type: "integer" },
+            },
+            required: ["foo"],
+            additionalProperties: false,
+        };
+        const data = { foo: 123, nestedBar: { a: 456, b: 789 } };
+        const result = maskData(schema, data, {});
+        const expected = { foo: 123 };
+        expect(result).toStrictEqual(expected);
+    });
+    test("it should mask a property with mismatched type by default", () => {
+        const schema = {
+            type: "object",
+            properties: {
+                foo: { type: "string" },
+            },
+            required: ["foo"],
+            additionalProperties: false,
+        };
+        const data = { foo: 123 };
+        const result = maskData(schema, data, {});
+        const expected = {};
+        expect(result).toStrictEqual(expected);
+    });
 
-    // test("it should not mask a property with mismatched type if configured in the options", () => {
-    //     const schema = {
-    //         type: "object",
-    //         properties: {
-    //             foo: { type: "string" },
-    //         },
-    //         required: ["foo"],
-    //         additionalProperties: false,
-    //     };
-    //     const data = { foo: 123 };
-    //     const result = maskData(schema, data, { shouldMaskTypeErrors: false });
-    //     const expected = { foo: 123 };
-    //     expect(result).toStrictEqual(expected);
-    // });
+    test("it should not mask a property with mismatched type if configured in the options", () => {
+        const schema = {
+            type: "object",
+            properties: {
+                foo: { type: "string" },
+            },
+            required: ["foo"],
+            additionalProperties: false,
+        };
+        const data = { foo: 123 };
+        const result = maskData(schema, data, { shouldMaskTypeErrors: false });
+        const expected = { foo: 123 };
+        expect(result).toStrictEqual(expected);
+    });
 
     test("it should call errorCallback function for missing properties if provided", () => {
         const schema = {
